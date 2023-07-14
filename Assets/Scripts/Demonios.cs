55f518412557b313e86cd1ea70e51e2978ff34cc
CHANCHUPU99 <106758212+CHANCHUPU99@users.noreply.github.com>
2023-07-13 20:15:22 -0600

    Mostrar elementos de los demons

--- Assets/Scripts/Demonios.cs
+++ Assets/Scripts/Demonios.cs
@@ -7,14 +7,14 @@ public class Demonios {
     private string name;
     private string description;
     private int knowLedge;
-    private int legions;
+    private string legions;
     private string direction;
     private string planet;
     private string metal;
     private string element;
     private string zodiaco;
 
-   public Demonios(string name, string description, int legions, string direction, string planet, string metal, string element, string zodiaco) {
+   public Demonios(string name, string description, string legions, string direction, string planet, string metal, string element, string zodiaco) {
         this.name = name;
         this.description = description;
         this.legions = legions;
@@ -25,18 +25,16 @@ public class Demonios {
         this.zodiaco = zodiaco;
     }
 
- 
-
     public string Name { get { return name; } set { name = value; } }
     public string Description { get { return description; } set { description = value; } }
-    public int Legions { get { return legions; } set { legions = value; } }
+    public string Legions { get { return legions; } set { legions = value; } }
     public string Direction { get { return direction; } set { direction = value; } }
     public string Planet { get { return planet; } set { planet = value; } }
     public string Metal { get { return metal; } set { metal = value; } }
     public string Element { get { return element; } set { element = value; } }
     public string Zodiaco { get { return zodiaco; } set { zodiaco = value; } }
 
-    public void printDemonElements() {
-        Debug.Log(name  + description + legions + Direction + planet + metal + element + zodiaco);
+    public string demonElements() {
+        return name + description + legions + Direction + planet + metal + element + zodiaco;
     }
 }
--- Assets/Scripts/DemonsManager.cs
+++ Assets/Scripts/DemonsManager.cs
@@ -5,25 +5,34 @@ using UnityEngine;
 
 public class DemonsManager : MonoBehaviour
 {
-  
+
+    Demonios m_demonnios;
     void Start()
     {
-        Demonios demon0 = new Demonios("bael", "El primer Rey del Infierno",66,"sur","Sol","oro","Fuego","Aries");
-        Demonios demon1 = new Demonios("Agares", "puede  ayudar a  los  fugitivos  a  escapar  y  enseña expresiones inmorales, además de acabar con la dignidad de alguien", 31,"sur","Venus","Cobre","tierra","Aries" );
-        Demonios demon2 = new Demonios("Vassago", "Puede conocer el pasado y el futuro y además posee la capacidad de encontrar objetos escondidos o perdidos", 26, "Oeste", "Jupiter","Estaño","agua", "Aries");
-        Demonios demon3 = new Demonios("Samigina", "Enseña ciencias liberales y da cuenta de las almas que murieron en pecado y de los que se ahogaron en el mar",30,"Oeste","Luna","Plata","agua","Aries");
-        Demonios demon4 = new Demonios("Marbas", " Posee conocimientos sobre lo oculto y enseña artes mecanicas",36,"Este","Mercurio","Mercurio","Aire","Aries");
-        Demonios demon5 = new Demonios("Valefor", "una de sus misiones principales es la de tentar a robar a las personas",10,"Norte", "Venus","cobre","Tierra","Aries");
-        Demonios demon6 = new Demonios("AAmon", "Conoce hechos pasados y futuros y puede ayudar a recon-ciliar a enemigos aunque también todo lo contrario",40,"Oeste","luna","Plata","Agua","Tauro");
-        Demonios demon7 = new Demonios("Barbatos", "Conoce pasado y futuro, reconcilia a amigos en conflicto y da la capacidad de entender las lenguas de los animales",30, "Sur", "Venus", "cobre" ,"Fuego", "Tauro");
-        Demonios demon8 = new Demonios("Paimon", "Paimon enseña todas las artes, las filosofías, las ciencias y las cosas secretas; puede revelar todos los misterios de la tierra , el viento y el agua, lo que es la mente y todo lo que el conjurador quiera saber" , 200 , "Oeste","Sol","Oro","Agua","Tauro");
-        Demonios demon9 = new Demonios("Buer", "Aparece cuando el Sol está en Sagitario. Al igual que Quirón, el centauro principal de la mitología griega, enseña filosofía natural y moral, lógica y las virtudes de todas las hierbas y plantas, y también es capaz de curar todas las enfermedades" , 50 , "Sur" , "Mercurio" , "Mercurio","Fuego","Tauro");
-        Demonios demon10 = new Demonios("Guison", "Conoce todo lo referente al pasado, presente y futuro, responde todas las preguntas que se le hagan, reconcilia a los amigos y otorga honor y dignidad.",40,"Oeste","Venus","Cobre","Agua","Tauro");
+       
+        Demonios demon0 = new Demonios("bael", "El primer Rey del Infierno","Sesenta y seis","sur","Sol","oro","Fuego","Aries");
+        Demonios demon1 = new Demonios("Agares", "puede  ayudar a  los  fugitivos  a  escapar  y  enseña expresiones inmorales, además de acabar con la dignidad de alguien", "Treinta y uno" ,"sur","Venus","Cobre","tierra","Aries" );
+        Demonios demon2 = new Demonios("Vassago", "Puede conocer el pasado y el futuro y además posee la capacidad de encontrar objetos escondidos o perdidos", "Veintiséis" , "Oeste", "Jupiter","Estaño","agua", "Aries");
+        Demonios demon3 = new Demonios("Samigina", "Enseña ciencias liberales y da cuenta de las almas que murieron en pecado y de los que se ahogaron en el mar", "Treinta" ,"Oeste","Luna","Plata","agua","Aries");
+        Demonios demon4 = new Demonios("Marbas", " Posee conocimientos sobre lo oculto y enseña artes mecanicas", "Treinta y seis", "Este","Mercurio","Mercurio","Aire","Aries");
+        Demonios demon5 = new Demonios("Valefor", "una de sus misiones principales es la de tentar a robar a las personas", "Diez" ,"Norte", "Venus","cobre","Tierra","Aries");
+        Demonios demon6 = new Demonios("AAmon", "Conoce hechos pasados y futuros y puede ayudar a recon-ciliar a enemigos aunque también todo lo contrario", "Cuarenta" ,"Oeste","luna","Plata","Agua","Tauro");
+        Demonios demon7 = new Demonios("Barbatos", "Conoce pasado y futuro, reconcilia a amigos en conflicto y da la capacidad de entender las lenguas de los animales", "Treinta" , "Sur", "Venus", "cobre" ,"Fuego", "Tauro");
+        Demonios demon8 = new Demonios("Paimon", "Paimon enseña todas las artes, las filosofías, las ciencias y las cosas secretas; puede revelar todos los misterios de la tierra , el viento y el agua, lo que es la mente y todo lo que el conjurador quiera saber" , "Doscientos" , "Oeste","Sol","Oro","Agua","Tauro");
+        Demonios demon9 = new Demonios("Buer", "Aparece cuando el Sol está en Sagitario. Al igual que Quirón, el centauro principal de la mitología griega, enseña filosofía natural y moral, lógica y las virtudes de todas las hierbas y plantas, y también es capaz de curar todas las enfermedades" , "Cincuenta" , "Sur" , "Mercurio" , "Mercurio","Fuego","Tauro");
+        Demonios demon10 = new Demonios("Guison", "Conoce todo lo referente al pasado, presente y futuro, responde todas las preguntas que se le hagan, reconcilia a los amigos y otorga honor y dignidad.", "Cuarenta" ,"Oeste","Venus","Cobre","Agua","Tauro");
+
+        print(demon0.demonElements());
+        print(demon1.demonElements());
+        print(demon2.demonElements());
+        print(demon3.demonElements());
+        print(demon4.demonElements());
+        print(demon5.demonElements());
+        print(demon6.demonElements());            
+        print(demon7.demonElements());
+        print(demon8.demonElements());
+        print(demon9.demonElements());
+        print(demon10.demonElements());
     }
 
-    
-    void Update()
-    {
-        
-    }
 }
