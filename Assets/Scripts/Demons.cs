5e9fe920c837207fd6abea701e03a1bf7e6a8c3b
04WS-LAB202-06\Alumno <rafael.cano@artek.edu.mx>
2023-07-21 08:38:16 -0600

    creacion interfaz

---
+++ Assets/Scripts/Demons.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Demons : MonoBehaviour
+{
+    public abstract string getName();
+    public abstract string getLegions();
+    public abstract string getDirection();
+    public abstract string getPlanet();
+    public abstract string getMetal();
+    public abstract string getElement();
+    public abstract string getZodiaco();
+    public abstract void setName(string name);
+    public abstract void setDirection(string direction);
+    public abstract void setLegions(string legions);
+    public abstract void setPlanet(string planet);
+    public abstract void setMetal(string metal);
+    public abstract void setElement(string element);
+    public abstract void setZodiaco(string zodiaco);
+}
+
+public class InterfaceDemon : Demons
+{
+    public override string getName()
+    {
+        return name;
+    }
+    public override string getLegions()
+    {
+        return legions;
+    }
+    public override string getDirection()
+    {
+        return direction;
+    }
+    public override string getPlanet()
+    {
+        return planet;
+    }
+    public override string getMetal()
+    {
+        return metal;
+    }
+    public override string getElement()
+    {
+        return element;
+    }
+    public override string getZodiaco()
+    {
+        return zodiaco;
+    }
+    public override void setName(string name)
+    {
+        this.name = name;
+    }
+    public override void setDirection(string direction)
+    {
+        this.direction = direction;
+    }
+    public override void setLegions(string legions)
+    {
+        this.legions = legions;
+    }
+    public override void setPlanet(string planet)
+    {
+        this.planet = planet;
+    }
+    public override void setMetal(string metal)
+    {
+        this.metal = metal;
+    }
+    public override void setElement(string element)
+    {
+        this.element = element;
+    }
+    public override void setZodiaco(string zodiaco)
+    {
+        this.zodiaco = zodiaco;
+    }
+
+    private string name;
+    private string description;
+    private string legions;
+    private string direction;
+    private string planet;
+    private string metal;
+    private string element;
+    private string zodiaco;
+
+    public InterfaceDemon(string name, string description, string legions, string direction, string planet, string metal, string element, string zodiaco)
+    {
+        this.name = name;
+        this.description = description;
+        this.legions = legions;
+        this.direction = direction;
+        this.planet = planet;
+        this.metal = metal;
+        this.element = element;
+        this.zodiaco = zodiaco;
+    }
+
+    //public string Name { get { return name; } set { name = value; } }
+    //public string Description { get { return description; } set { description = value; } }
+    //public string Legions { get { return legions; } set { legions = value; } }
+    //public string Direction { get { return direction; } set { direction = value; } }
+    //public string Planet { get { return planet; } set { planet = value; } }
+    //public string Metal { get { return metal; } set { metal = value; } }
+    //public string Element { get { return element; } set { element = value; } }
+    //public string Zodiaco { get { return zodiaco; } set { zodiaco = value; } }
+
+    public string demonElements()
+    {
+        return getName() + getLegions();
+        //return name + description + legions + Direction + planet + metal + element + zodiaco;
+    }
+}
--- Assets/Scripts/DemonsManager.cs
+++ Assets/Scripts/DemonsManager.cs
@@ -10,17 +10,17 @@ public class DemonsManager : MonoBehaviour
     void Start()
     {
        
-        Demonios demon0 = new Demonios("bael", "El primer Rey del Infierno","Sesenta y seis","sur","Sol","oro","Fuego","Aries");
-        Demonios demon1 = new Demonios("Agares", "puede  ayudar a  los  fugitivos  a  escapar  y  enseña expresiones inmorales, además de acabar con la dignidad de alguien", "Treinta y uno" ,"sur","Venus","Cobre","tierra","Aries" );
-        Demonios demon2 = new Demonios("Vassago", "Puede conocer el pasado y el futuro y además posee la capacidad de encontrar objetos escondidos o perdidos", "Veintiséis" , "Oeste", "Jupiter","Estaño","agua", "Aries");
-        Demonios demon3 = new Demonios("Samigina", "Enseña ciencias liberales y da cuenta de las almas que murieron en pecado y de los que se ahogaron en el mar", "Treinta" ,"Oeste","Luna","Plata","agua","Aries");
-        Demonios demon4 = new Demonios("Marbas", " Posee conocimientos sobre lo oculto y enseña artes mecanicas", "Treinta y seis", "Este","Mercurio","Mercurio","Aire","Aries");
-        Demonios demon5 = new Demonios("Valefor", "una de sus misiones principales es la de tentar a robar a las personas", "Diez" ,"Norte", "Venus","cobre","Tierra","Aries");
-        Demonios demon6 = new Demonios("AAmon", "Conoce hechos pasados y futuros y puede ayudar a recon-ciliar a enemigos aunque también todo lo contrario", "Cuarenta" ,"Oeste","luna","Plata","Agua","Tauro");
-        Demonios demon7 = new Demonios("Barbatos", "Conoce pasado y futuro, reconcilia a amigos en conflicto y da la capacidad de entender las lenguas de los animales", "Treinta" , "Sur", "Venus", "cobre" ,"Fuego", "Tauro");
-        Demonios demon8 = new Demonios("Paimon", "Paimon enseña todas las artes, las filosofías, las ciencias y las cosas secretas; puede revelar todos los misterios de la tierra , el viento y el agua, lo que es la mente y todo lo que el conjurador quiera saber" , "Doscientos" , "Oeste","Sol","Oro","Agua","Tauro");
-        Demonios demon9 = new Demonios("Buer", "Aparece cuando el Sol está en Sagitario. Al igual que Quirón, el centauro principal de la mitología griega, enseña filosofía natural y moral, lógica y las virtudes de todas las hierbas y plantas, y también es capaz de curar todas las enfermedades" , "Cincuenta" , "Sur" , "Mercurio" , "Mercurio","Fuego","Tauro");
-        Demonios demon10 = new Demonios("Guison", "Conoce todo lo referente al pasado, presente y futuro, responde todas las preguntas que se le hagan, reconcilia a los amigos y otorga honor y dignidad.", "Cuarenta" ,"Oeste","Venus","Cobre","Agua","Tauro");
+        InterfaceDemon demon0 = new InterfaceDemon("bael", "El primer Rey del Infierno","Sesenta y seis","sur","Sol","oro","Fuego","Aries");
+        InterfaceDemon demon1 = new InterfaceDemon("Agares", "puede  ayudar a  los  fugitivos  a  escapar  y  enseña expresiones inmorales, además de acabar con la dignidad de alguien", "Treinta y uno" ,"sur","Venus","Cobre","tierra","Aries" );
+        InterfaceDemon demon2 = new InterfaceDemon("Vassago", "Puede conocer el pasado y el futuro y además posee la capacidad de encontrar objetos escondidos o perdidos", "Veintiséis" , "Oeste", "Jupiter","Estaño","agua", "Aries");
+        InterfaceDemon demon3 = new InterfaceDemon("Samigina", "Enseña ciencias liberales y da cuenta de las almas que murieron en pecado y de los que se ahogaron en el mar", "Treinta" ,"Oeste","Luna","Plata","agua","Aries");
+        InterfaceDemon demon4 = new InterfaceDemon("Marbas", " Posee conocimientos sobre lo oculto y enseña artes mecanicas", "Treinta y seis", "Este","Mercurio","Mercurio","Aire","Aries");
+        InterfaceDemon demon5 = new InterfaceDemon("Valefor", "una de sus misiones principales es la de tentar a robar a las personas", "Diez" ,"Norte", "Venus","cobre","Tierra","Aries");
+        InterfaceDemon demon6 = new InterfaceDemon("AAmon", "Conoce hechos pasados y futuros y puede ayudar a recon-ciliar a enemigos aunque también todo lo contrario", "Cuarenta" ,"Oeste","luna","Plata","Agua","Tauro");
+        InterfaceDemon demon7 = new InterfaceDemon("Barbatos", "Conoce pasado y futuro, reconcilia a amigos en conflicto y da la capacidad de entender las lenguas de los animales", "Treinta" , "Sur", "Venus", "cobre" ,"Fuego", "Tauro");
+        InterfaceDemon demon8 = new InterfaceDemon("Paimon", "Paimon enseña todas las artes, las filosofías, las ciencias y las cosas secretas; puede revelar todos los misterios de la tierra , el viento y el agua, lo que es la mente y todo lo que el conjurador quiera saber" , "Doscientos" , "Oeste","Sol","Oro","Agua","Tauro");
+        InterfaceDemon demon9 = new InterfaceDemon("Buer", "Aparece cuando el Sol está en Sagitario. Al igual que Quirón, el centauro principal de la mitología griega, enseña filosofía natural y moral, lógica y las virtudes de todas las hierbas y plantas, y también es capaz de curar todas las enfermedades" , "Cincuenta" , "Sur" , "Mercurio" , "Mercurio","Fuego","Tauro");
+        //Demonios demon10 = new Demonios("Guison", "Conoce todo lo referente al pasado, presente y futuro, responde todas las preguntas que se le hagan, reconcilia a los amigos y otorga honor y dignidad.", "Cuarenta" ,"Oeste","Venus","Cobre","Agua","Tauro");
 
         print(demon0.demonElements());
         print(demon1.demonElements());
@@ -32,7 +32,7 @@ public class DemonsManager : MonoBehaviour
         print(demon7.demonElements());
         print(demon8.demonElements());
         print(demon9.demonElements());
-        print(demon10.demonElements());
+        //print(demon10.demonElements());
     }
 
 }
