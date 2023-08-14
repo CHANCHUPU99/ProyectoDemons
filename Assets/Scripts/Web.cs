e400e327c53b7a0e7dbc7938f12ddc1712f3aa58
CHANCHUPU99 <106758212+CHANCHUPU99@users.noreply.github.com>
2023-08-14 13:48:43 -0600

    descarga Vr

---
+++ Assets/Scripts/Web.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Web : MonoBehaviour
+{
+    void Start() {
+        // A correct website page.
+        StartCoroutine(GetRequest("http://172.100.1.153:3000/"));
+
+        // A non-existing page.
+        //StartCoroutine(GetRequest("https://error.html"));
+    }
+
+    IEnumerator GetRequest(string uri) {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)) {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result) {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    break;
+            }
+        }
+    }
+}
