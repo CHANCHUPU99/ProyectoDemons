c37791bd78220eafa1fa878f909171b1621e5389
CHANCHUPU99 <106758212+CHANCHUPU99@users.noreply.github.com>
2023-08-21 13:35:32 -0600

    carta prueba

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QVlHS3RVYi8vLy8vQUFBQm1hK3ljbHBDOWsvTGlNek9NdVVHcHY5M08xU2gzbjNEZW4zTytkbXZweWFTc3ByNy9kc0pQY3JmeTA2U3NzMWhkOEdxZlJzN2tIRjQ4QmRhNFJTSTZBaUZsUlNkU3FWMGxHQmU4RVNnWEJ4MDUzTmM0L0xrb3ltazN5MDQzRmN5Tm5lVklXR05aTFlsRVVjMXVrbERMcFNFMDIwVStHY1d6OGx5TE82YXl5MWQxZ0RzOUlmZkExMmtmbnFNMmV6SFBvQXZUOWQ3TG9mTmhYS2FPcTZtamFzekdta2VpQWpxUkZBb1Zyc2JTczYzbUp6YnF3Z1RJSnZZbFg5TGtZQndDV052alE0K3ZLYVJPTTY5RC9sa2s3cnVDK2JoQ1FFQ2lyZUhBek1HSVZ3SWdmUFV0ZDNtcGd1cVN5aUhDdHd3a0Jmb3ZvQnl4VU51amxrYnJSQVgrQkk2bUtZMG5nVFBDVGE4aDRaVmdWRk0=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
